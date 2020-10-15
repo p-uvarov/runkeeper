b6e4fcc2761db9113b39ec66766f9e81aafc09f4
Pavel Uvarov <pavel.uvarov@thinkdirtyapp.com>
2020-10-15 17:41:03 -0400

    Initial Commit

---
+++ Runkeeper/RunkeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunkeeperApp.swift
+//  Runkeeper
+//
+//  Created by Pavel Uvarov on 2020-10-15.
+//
+
+import SwiftUI
+
+@main
+struct RunkeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
